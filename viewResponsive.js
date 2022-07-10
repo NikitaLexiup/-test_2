3f03e07d378a6d76a32145f090179ded514269e9
Nikita Ivanov <106706979+NikitaLexiup@users.noreply.github.com>
2022-07-10 21:57:54 +0300

    Add files via upload

--- viewResponsive.js
+++ viewResponsive.js
@@ -1,36 +1,39 @@
 "use strict"
-const desktopView = document.querySelector('.content_project_block_desktop');
-const tabletView = document.querySelector('.content_project_block_tablet');
-const phoneView = document.querySelector('.content_project_block_phone');
-const desktopViewBtn = document.querySelector('.desktop_view');
-const tabletViewBtn = document.querySelector('.tablet_view');
-const phoneViewBtn = document.querySelector('.phone_view');
 
+const projects = document.querySelectorAll(".project_conteiner")
 
-if (desktopViewBtn, tabletViewBtn, phoneViewBtn) {
-  desktopViewBtn.addEventListener("click", function (e) {
-    desktopView.classList.toggle('_active', true);
-    desktopView.classList.toggle('_default', true);
-    tabletView.classList.remove('_active');
-    phoneView.classList.remove('_active');
-  });
-  tabletViewBtn.addEventListener("click", function (e) {
-    desktopView.classList.remove('_active');
-    desktopView.classList.toggle('_default', true);
-    tabletView.classList.toggle('_active', true);
-    phoneView.classList.remove('_active');
-  });
-  phoneViewBtn.addEventListener("click", function (e) {
-    desktopView.classList.remove('_active');
-    desktopView.classList.toggle('_default', true);
-    tabletView.classList.remove('_active');
-    phoneView.classList.toggle('_active', true);
-  });
+for (const project of projects) {
+  console.log(project)
+
+  const desktopView = project.querySelector(".content_project_block_desktop")
+  const tabletView = project.querySelector(".content_project_block_tablet")
+  const phoneView = project.querySelector(".content_project_block_phone")
+  const desktopViewBtn = project.querySelector(".desktop_view")
+  const tabletViewBtn = project.querySelector(".tablet_view")
+  const phoneViewBtn = project.querySelector(".phone_view")
+
+  if ((desktopViewBtn, tabletViewBtn, phoneViewBtn)) {
+    desktopViewBtn.addEventListener("click", function (e) {
+      desktopView.classList.toggle("_active", true)
+      desktopView.classList.toggle("_default", true)
+      tabletView.classList.remove("_active")
+      phoneView.classList.remove("_active")
+    })
+    tabletViewBtn.addEventListener("click", function (e) {
+      desktopView.classList.remove("_active")
+      desktopView.classList.toggle("_default", true)
+      tabletView.classList.toggle("_active", true)
+      phoneView.classList.remove("_active")
+    })
+    phoneViewBtn.addEventListener("click", function (e) {
+      desktopView.classList.remove("_active")
+      desktopView.classList.toggle("_default", true)
+      tabletView.classList.remove("_active")
+      phoneView.classList.toggle("_active", true)
+    })
+  }
 }
 
-const btnSearch = document.querySelectorAll("#btn");
-
-console.log(btnSearch);
 
 
 
