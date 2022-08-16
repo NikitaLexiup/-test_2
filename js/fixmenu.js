4c252d2fe4e6c0d5f28ffbc8e93fc15b682d12c2
Nikita Ivanov <106706979+NikitaLexiup@users.noreply.github.com>
2022-08-17 02:05:07 +0300

    Add files via upload (#10)

--- js/fixmenu.js
+++ js/fixmenu.js
@@ -2,9 +2,9 @@ window.onscroll = () => {
     const nav = document.querySelector ('.header')
     const Y = window.scrollY
     
-    if(Y > 50) {
+    if(Y > 30) {
     nav.classList.add ('header_fixed')
-    } else if(Y < 50) {
+    } else if(Y < 30) {
       nav.classList.remove ('header_fixed')
       }
   }
